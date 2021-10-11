1940b73945385c988329f4feb431470b3028d899
flawlesscode254 <denzme414@gmail.com>
2021-10-11 13:42:49 +0300

    worked on the meet screen

--- screens/MeetScreen.js
+++ screens/MeetScreen.js
@@ -1,10 +1,58 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {} from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 const MeetScreen = () => {
     return (
-        <View>
-            <Text>Google Meet</Text>
+        <View style={{
+            backgroundColor: "white",
+            flex: 1,
+            paddingHorizontal: 20,
+            paddingVertical: 20
+        }}>
+        <View style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center"
+        }}>
+            <TouchableOpacity style={{
+                paddingVertical: 5,
+                borderRadius: 5,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#095ee6",
+                width: 150
+            }}>
+                <Text style={{
+                    color: "white",
+                    letterSpacing: 1,
+                    fontWeight: "bold"
+                }}>New Meeting</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{
+                paddingVertical: 5,
+                borderRadius: 5,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "white",
+                borderWidth: 0.5,
+                borderColor: "grey",
+                width: 150
+            }}>
+                <Text style={{
+                    color: "#095ee6",
+                    letterSpacing: 1,
+                    fontWeight: "bold"
+                }}>Join with a code</Text>
+            </TouchableOpacity>
+        </View>
+            <Image 
+                source={(require("../assets/call.png"))}
+                style={{
+                    width: "100%",
+                    height: 200,
+                    marginTop: 100
+                }}
+            />
         </View>
     )
 }
--- stacks/MainStack.js
+++ stacks/MainStack.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {Ionicons} from "@expo/vector-icons"
+import { Ionicons } from "@expo/vector-icons";
 
 import GmailScreen from "../screens/GmailScreen";
 import MeetScreen from "../screens/MeetScreen";
@@ -11,73 +11,73 @@ const MainStack = () => {
 
   const screenOptions = ({ route }) => ({
     tabBarIcon: ({ focused }) => {
-        let iconName = "mail"
-        switch (route.name) {
-            case "Mail":
-                iconName = "mail";
-                break;
-            
-            case "Meet":
-                iconName = "videocam";
-                break;
+      let iconName = "mail";
+      switch (route.name) {
+        case "Mail":
+          iconName = "mail";
+          break;
 
-            default:
-                iconName = "mail"
-        }
+        case "Meet":
+          iconName = "videocam";
+          break;
 
-        return (
-            <Ionicons 
-                name={focused ? iconName : `${iconName}-outline`}
-                color={focused ? "red" : "black"}
-                size={focused ? 30 : 25}
-            />
-        )
-    }
-})
+        default:
+          iconName = "mail";
+      }
+
+      return (
+        <Ionicons
+          name={focused ? iconName : `${iconName}-outline`}
+          color={focused ? "red" : "black"}
+          size={focused ? 30 : 25}
+        />
+      );
+    },
+  });
 
   return (
-    <Stack.Navigator
-      screenOptions={screenOptions}
-    >
-      <Stack.Screen 
-        name="Mail" 
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Mail"
         component={GmailScreen}
         options={{
-          headerShown: false
+          headerShown: false,
         }}
       />
-      <Stack.Screen 
-        name="Meet" 
+      <Stack.Screen
+        name="Meet"
         component={MeetScreen}
         options={{
           headerTitle: () => {
             return (
-              <Text style={{
-                fontSize: 20,
-                letterSpacing: 1
-              }}>Meet</Text>
-            )
+              <Text
+                style={{
+                  fontSize: 20,
+                  letterSpacing: 1,
+                }}
+              >
+                Meet
+              </Text>
+            );
           },
           headerTitleAlign: "center",
           headerLeft: () => {
-            return (
-              <Ionicons name="menu" color="black" size={28} />
-            )
+            return <Ionicons name="menu" color="black" size={28} />;
           },
           headerRight: () => {
             return (
-              <Image 
+              <Image
                 source={{
-                  uri: "https://i0.wp.com/css-tricks.com/wp-content/uploads/2021/02/firebase-logo-icon.png?fit=1200%2C600&ssl=1"
+                  uri: "https://i0.wp.com/css-tricks.com/wp-content/uploads/2021/02/firebase-logo-icon.png?fit=1200%2C600&ssl=1",
                 }}
                 style={{
                   width: 40,
                   height: 40,
                   borderRadius: 999,
-                  marginRight: 10
+                  marginRight: 10,
                 }}
               />
-            )
+            );
           }
         }}
       />
