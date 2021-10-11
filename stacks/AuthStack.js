a34d846f1550489131b40c283a585d5c01c55398
flawlesscode254 <denzme414@gmail.com>
2021-10-11 13:08:43 +0300

    added authentication

--- stacks/AuthStack.js
+++ stacks/AuthStack.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import SignInScreen from '../screens/SignInScreen'
+import SignUpScreen from '../screens/SignUpScreen'
 
 const AuthStack = () => {
+
+    const Stack = createNativeStackNavigator()
+
     return (
-        <View>
-            <Text>Authentication</Text>
-        </View>
+        <Stack.Navigator
+            screeenOptions={{
+                headerShown: false
+            }}
+        >
+            <Stack.Screen 
+                name="SignIn"
+                component={SignInScreen}
+            />
+            <Stack.Screen 
+                name="SignUp"
+                component={SignUpScreen}
+            />
+        </Stack.Navigator>
     )
 }
 
--- stacks/MainStack.js
+++ stacks/MainStack.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { Text, Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {Ionicons} from "@expo/vector-icons"
 
 import GmailScreen from "../screens/GmailScreen";
 import MeetScreen from "../screens/MeetScreen";
@@ -7,14 +9,78 @@ import MeetScreen from "../screens/MeetScreen";
 const MainStack = () => {
   const Stack = createBottomTabNavigator();
 
+  const screenOptions = ({ route }) => ({
+    tabBarIcon: ({ focused }) => {
+        let iconName = "mail"
+        switch (route.name) {
+            case "Mail":
+                iconName = "mail";
+                break;
+            
+            case "Meet":
+                iconName = "videocam";
+                break;
+
+            default:
+                iconName = "mail"
+        }
+
+        return (
+            <Ionicons 
+                name={focused ? iconName : `${iconName}-outline`}
+                color={focused ? "red" : "black"}
+                size={focused ? 30 : 25}
+            />
+        )
+    }
+})
+
   return (
     <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
+      screenOptions={screenOptions}
     >
-      <Stack.Screen name="Mail" component={GmailScreen} />
-      <Stack.Screen name="Meet" component={MeetScreen} />
+      <Stack.Screen 
+        name="Mail" 
+        component={GmailScreen}
+        options={{
+          headerShown: false
+        }}
+      />
+      <Stack.Screen 
+        name="Meet" 
+        component={MeetScreen}
+        options={{
+          headerTitle: () => {
+            return (
+              <Text style={{
+                fontSize: 20,
+                letterSpacing: 1
+              }}>Meet</Text>
+            )
+          },
+          headerTitleAlign: "center",
+          headerLeft: () => {
+            return (
+              <Ionicons name="menu" color="black" size={28} />
+            )
+          },
+          headerRight: () => {
+            return (
+              <Image 
+                source={{
+                  uri: "https://i0.wp.com/css-tricks.com/wp-content/uploads/2021/02/firebase-logo-icon.png?fit=1200%2C600&ssl=1"
+                }}
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 999,
+                  marginRight: 10
+                }}
+              />
+            )
+          }
+        }}
+      />
     </Stack.Navigator>
   );
 };
